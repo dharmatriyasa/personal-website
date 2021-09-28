5fcaf181b3ed0677c64fc95946a50199446538bc
dharmatriyasa <dharmatriyasa19@gmail.com>
2021-09-28 12:17:20 +0800

    animate website

---
+++ js/jquery.js
@@ -0,0 +1,23 @@
+$("#btn-email").on("click", function (e) {
+    e.preventDefault();
+    const href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
+});
+
+$("#about-click").on("click", function (e) {
+    e.preventDefault();
+    const href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
+});
+
+$("#meet-click").on("click", function (e) {
+    e.preventDefault();
+    const href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
+});
+
+$("#contact-click").on("click", function (e) {
+    e.preventDefault();
+    const href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
+});
